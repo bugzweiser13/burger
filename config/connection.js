85a6ab5489570cdd7ab20a295b1eb2ea92c84c21
bugzweiser <bugzweiser@gmail.com>
2019-07-27 10:33:13 -0700

    database creation

--- config/connection.js
+++ config/connection.js
@@ -20,5 +20,16 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    });
+};
+
 // Export the connection.
 module.exports = connection;
