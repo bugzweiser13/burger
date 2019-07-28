28a1253a90f1dfd47ffd4c33407e592c8fc25a72
bugzweiser <bugzweiser@gmail.com>
2019-07-27 17:41:07 -0700

    server correction

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 // Dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
